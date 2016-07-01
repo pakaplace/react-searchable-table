8380986e7850802c11cbc61ab576cd80c67d286f
Mustafa Paksoy <paksoy@gmail.com>
2016-07-01 15:40:56 -0400

    Add csv columns example.

--- csv-example.js
+++ csv-example.js
@@ -6,3 +6,7 @@ var data = fs.readFileSync('sample.csv', {encoding: 'utf8'});
 parse(data, {}, function(err, data) {
   console.log('err', err, 'data', data);
 });
+
+parse(data, {columns: true}, function(err, data) {
+  console.log('columns err', err, 'data', data);
+});
