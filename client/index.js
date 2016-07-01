6689501880377c04631f0a50593a7a0ed28a4034
Mustafa Paksoy <paksoy@gmail.com>
2016-07-01 15:07:39 -0400

    Move file input to own file.

--- client/index.js
+++ client/index.js
@@ -1,22 +1,8 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var FileInput = require('./file-input');
 
-var FileInput = React.createClass({
-  onChange: function(event) {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    reader.onload = this.onFileRead;
-    reader.readAsText(file);
-  },
-  onFileRead: function(event) {
-    this.props.onChange(event.target.result);
-  },
-  render: function() {
-    return <input type="file" onChange={this.onChange} />;
-  }
-});
-
-var Master = React.createClass({
+var App = React.createClass({
   getInitialState: function() {
     return {
       text: ''
@@ -32,11 +18,12 @@ var Master = React.createClass({
       <form>
         <div className="form-group">
           <FileInput className="form-control" onChange={this.onChange} />
+            <p className="help-block">Select a CSV file.</p>
         </div>
-        <textarea disabled={true} className="form-control" value={this.state.text} />
+        <textarea disabled="true" className="form-control" value={this.state.text} />
       </form>
     );
   }
 });
 
-ReactDOM.render(<Master/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
